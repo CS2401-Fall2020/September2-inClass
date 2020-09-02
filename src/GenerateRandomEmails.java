66020ded4ec3e208f28fb093454dd375f81cca52
Dan DeBlasio <dan@dandeblasio.com>
2020-09-02 11:49:09 -0600

    Working on this in class. Final result is this commit.

--- src/GenerateRandomEmails.java
+++ src/GenerateRandomEmails.java
@@ -1,16 +1,22 @@
 import java.util.Random;
 
 public class GenerateRandomEmails {
-  private static String[] domains = {"gmail.com","live.com","miners.utep.edu","utep.edu","google.com","hotmail.com"};
+  private static String[] domains = {
+      "gmail.com",
+      "miners.utep.edu",
+      //"utep.edu",
+      //"google.com",
+      "hotmail.com",
+      "deblasio.cc"};
   
   public static String[] generateRandomEmails(int number) {
     String[] rtn = new String[number];
-    Random random = new Random();
+    Random random = new Random(4962768);
     for(int i=0; i<number; i++) {
-      int randLength = random.nextInt(15) + 5;
+      int randLength = random.nextInt(8) + 4;
       int randDomain = random.nextInt(domains.length);
       StringBuilder buffer = new StringBuilder(randLength + 1 + domains[randDomain].length());
-      buffer.append(randomString(randLength));
+      buffer.append(randomString(randLength, random.nextInt()));
       buffer.append('@');
       buffer.append(domains[randDomain]);
       rtn[i] = buffer.toString();
@@ -18,16 +24,25 @@ public class GenerateRandomEmails {
     return rtn;
   }
   
-  private static String randomString(int len) {
-    int leftLimit = 97; // letter 'a'
-    int rightLimit = 122; // letter 'z'
+  private static String randomString(int len, int randomLong) {
+    //int leftLimit = 97; // letter 'a'
+    //int rightLimit = 122; // letter 'z'
+    char[] validChars = new char[26*2+10];
+    for(int i=0;i<26;i++) {
+      validChars[i] = (char)(97 + i);
+      validChars[26+i] = (char)(65 + i);
+    }
+    for(int i=0; i<10; i++) {
+      validChars[26*2 + i] = (char)(48 + i);
+    }
     int targetStringLength = len;
-    Random random = new Random();
+    Random random = new Random(randomLong);
     StringBuilder buffer = new StringBuilder(targetStringLength);
     for (int i = 0; i < targetStringLength; i++) {
-        int randomLimitedInt = leftLimit + (int) 
-          (random.nextFloat() * (rightLimit - leftLimit + 1));
-        buffer.append((char) randomLimitedInt);
+        //int randomLimitedInt = leftLimit + (int) 
+        //  (random.nextFloat() * (rightLimit - leftLimit + 1));
+        //buffer.append((char) randomLimitedInt);
+      buffer.append(validChars[random.nextInt(validChars.length)]);
     }
     String generatedString = buffer.toString();
     return generatedString;
--- src/ProcessEmails.java
+++ src/ProcessEmails.java
@@ -4,22 +4,94 @@ public class ProcessEmails {
 
   public static void main(String[] args) {
     
-    String[] emails = GenerateRandomEmails.generateRandomEmails(5);
+    // Collect email addresses
+    String[] emails = GenerateRandomEmails.generateRandomEmails(10);
 
+    // Print addresses to verify
+    // using a basic for loop
+    /*for(int i=0; i<emails.length; i++) {
+      System.out.println(emails[i]);
+    }*/
+    
+    // using an enhanced for loop
+    /*int i=0;
+    for(String singleEmail: emails) {
+      System.out.println(singleEmail + " - " + getDomainFromEmail(singleEmail) + " - " + countDomainInSubArray(emails,getDomainFromEmail(singleEmail), 0, i));
+      i++;
+    }*/
+    String[] uniq = uniqueDomains(emails);
+    for(String domain: uniq) {
+      System.out.println("Domain: " + domain);
+    }
+    
   }
   
   
-  
-  public String[] uniqueDomains(String[] emails) {
-    return new String[0];
+  // Given a list of email addresses, 
+  // return a new array of only the unique domains 
+  // (the number of unique domains is unknown to start, you will need to count them)
+  public static String[] uniqueDomains(String[] emails) {
+    Boolean[] firstAppear = new Boolean[emails.length];
+    for(int i=0; i<emails.length; i++) {
+      firstAppear[i] = (countDomainInSubArray(emails,getDomainFromEmail(emails[i]), 0, i) == 1);
+      //System.out.println(emails[i] + " - " + firstAppear[i]);
+    }
+    int numUniq = 0;
+    for(int i=0; i<emails.length; i++) {
+      if(firstAppear[i]) {
+        numUniq++;
+      }
+    }
+    System.out.println("Number of Unique: " + numUniq);
+    String[] uniqDomains = new String[numUniq];
+    
+    int uniqCounter = 0; 
+    for(int i=0; i<emails.length; i++) {
+      if(firstAppear[i]) {
+        uniqDomains[uniqCounter++] = getDomainFromEmail(emails[i]);
+      }
+    }
+    /*
+    int i = 0;
+    for(int j=0; j<numUniq; j++) {
+      while(!firstAppear[i]) {
+        i++;
+      }
+      uniqDomains[j] = getDomainFromEmail(emails[i++]);
+    }*/
+    
+    return uniqDomains;
   }
   
-  public int countDomainInSubArray(String[] emails, String domain, int start, int end) {
-    return -1;
+  // For a given range, and a search domain, 
+  // find the number of email addresses in the range (inclusive) with that domain
+  public static int countDomainInSubArray(String[] emails, String domain, int start, int end) {
+    int count = 0; 
+    for(int i=start; i<=end; i++) {
+      if(getDomainFromEmail(emails[i]).equals(domain)) {
+        count++;
+      }
+    }
+    return count;
   }
-  
-  public String getDomainFromEmail(String email) {
-    return "";
+
+  // Find the domain in a string (i.e. the substring after '@')
+  public static String getDomainFromEmail(String email) {
+    // .charAt()
+    // .length()
+    // .substring()
+    
+    int indexOfAt = 0;
+    // Afk0H@miners.utep.edu
+    //      ^
+    
+    // find it
+    while(email.charAt(indexOfAt) != '@') {
+      indexOfAt++;
+    }
+    
+    assert(email.charAt(indexOfAt) == '@');
+    return email.substring(indexOfAt + 1);
   }
 
 }
